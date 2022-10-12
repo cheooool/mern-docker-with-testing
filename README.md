867f60745cafaf918e7cc2906795c79fae05979d
cheooool <g.cheooool@gmail.com>
2022-10-12 20:59:08 +0900

    프로젝트 초기 세팅 docker, mongodb, express 기본 설정 작업

---
+++ README.md
@@ -0,0 +1,9 @@
+# Complete react developer
+
+[the-complete-modern-react-developer-2022](https://andrewbaisden.hashnode.dev/)의 과정을 따라 개발한 것입니다.
+
+## 사용 기술
+
+- Docker
+- MongoDB
+- Express
---
+++ backend-express-mongodb/src/models/Twitter.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+interface Twitter {
+  tweet: string;
+  img: string;
+}
+
+const schema = new Schema<Twitter>({
+  tweet: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+});
+
+const TwitterModel = model<Twitter>('contents', schema);
+
+export default TwitterModel;
