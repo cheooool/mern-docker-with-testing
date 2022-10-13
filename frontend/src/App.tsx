e9ac05ed4a5b507b751aa00b4ae3ace11b2053e0
cheooool <g.cheooool@gmail.com>
2022-10-13 18:29:36 +0900

    트윗 기능 작업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useTwitter } from './contexts/TwitterContext';
 import './App.css';
 
 function App() {
+  const {
+    data,
+    loading,
+    toggleComposeTweet,
+    handleToggleComposeTweet,
+    postTweet,
+    updateTweet,
+    deleteTweet,
+    tweet,
+    setTweet,
+  } = useTwitter();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={tweet}
+        onChange={(e) => setTweet(e.target.value)}
+        placeholder="메시지를 입력해주세요."
+      />
+      <button
+        onClick={() => {
+          postTweet();
+          setTweet('');
+        }}
+      >
+        New Tweet
+      </button>
+
+      <ul>
+        {data.map(
+          (item: { _id: string; tweet: string; img: string }, key: number) => (
+            <li key={key}>
+              {item.tweet}
+              <button onClick={() => updateTweet(item, 'Tweet updated')}>
+                Update
+              </button>
+              <button onClick={() => deleteTweet(item._id)}>Delete</button>
+            </li>
+          )
+        )}
+      </ul>
     </div>
   );
 }
